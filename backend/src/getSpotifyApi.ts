7574414b1e1515974afb116e0a690937e8be6176
Tim Ittermann <tim.ittermann@gmail.com>
2019-11-26 12:11:01 +0100

    Fix redirection

--- backend/src/getSpotifyApi.ts
+++ backend/src/getSpotifyApi.ts
@@ -4,7 +4,11 @@ import getSpotifyAppToken from "./getSpotifyAppToken";
 export default async function getSpotifyApi(clientId: string, clientSecret: string, redirectUri?: string) : Promise<SpotifyWebApi> {
     let token = await getSpotifyAppToken(clientId, clientSecret);
     if (redirectUri) {
-        redirectUri += "/api/login_callback"
+        let append = "api/login_callback";
+        if (redirectUri[redirectUri.length-1] !== "/") {
+            append += "/";
+        }
+        redirectUri += append;
     }
 
     let api = new SpotifyWebApi({
